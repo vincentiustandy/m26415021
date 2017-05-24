4a7abb19a8d9a2fb69b7096d557fbc5bf919c7d8
vincentandy <m26415021@john.petra.ac.id>
2017-05-24 08:17:28 +0700

    Create shapemanipulation.py

---
+++ PR/PR11/NumPy/shapemanipulation.py
@@ -0,0 +1,8 @@
+>>> import numpy as np
+>>> a = np.floor(10*np.random.random((3,4)))
+>>> a
+array([[ 2.,  8.,  0.,  6.],
+       [ 4.,  5.,  1.,  1.],
+       [ 8.,  9.,  3.,  6.]])
+>>> a.shape
+(3, 4)
